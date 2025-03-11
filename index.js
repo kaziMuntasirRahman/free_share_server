7a5ae7408b4f4be23828f2a7b6f29033892fcb32
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2025-03-11 21:36:02 +0600

    testing on conversation feature

--- index.js
+++ index.js
@@ -26,13 +26,45 @@ const client = new MongoClient(uri, {
 })
 
 const DB = client.db('FREE_SHARE_DB')
+const userCollection = DB.collection('user')
 const contentCollection = DB.collection('content')
+const conversationCollection = DB.collection('conversation')
 
 async function run () {
   try {
     // await client.connect()
     // await client.db('admin').command({ ping: 1 })
     console.log('Pinged your deployment. Successfully connected to MongoDB!')
+
+    // add new user
+    app.post('/users', async (req, res) => {
+      console.log('post /users api is hit...')
+      const { displayName, email, photoURL, password } = req.body
+      const newUser = {
+        displayName,
+        email,
+        photoURL,
+        password,
+        savedContent: [],
+        inbox: [],
+        outbox: [],
+        likedContent: []
+      }
+      const fetchedInbox = await conversationCollection
+        .find({ receiver: email })
+        .toArray()
+
+      newUser.inbox = fetchedInbox
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    // get a user
+    app.get('/users/:email', async (req, res) => {
+      console.log('get /users/:email api is hit...')
+      // const email = req.params.userEmail;
+      res.send(await userCollection.findOne({ email: req.params.email }))
+    })
 
     //post a new content
     app.post('/contents', async (req, res) => {
@@ -64,12 +96,15 @@ async function run () {
     })
 
     //add a new field
-    app.patch('/contents/add-field/:field_name', async (req, res)=>{
+    app.patch('/contents/add-field/:field_name', async (req, res) => {
       console.log('Patch /contents/add-field/:field_name api is hit...')
-      const {field_name} = req.params;
-      const result = await contentCollection.updateMany({},{$set: {[field_name]: []}})
+      const { field_name } = req.params
+      const result = await contentCollection.updateMany(
+        {},
+        { $set: { [field_name]: [] } }
+      )
       res.send(result)
-    }) 
+    })
 
     //remove a field
     app.patch('/contents/remove-field/:field_name', async (req, res) => {
@@ -81,7 +116,22 @@ async function run () {
       )
       res.send(result)
     })
-    
+
+    // handle content share
+    app.post('/conversations', async (req, res) => {
+      console.log('Post /conversation api is hit...')
+      const { content, sender, receiver, isAnonymous } = req.body
+      const sendTime = new Date().toISOString()
+      const result = await conversationCollection.insertOne({
+        content,
+        sender,
+        receiver,
+        isAnonymous,
+        sendTime
+      })
+      // console.log(result)
+      res.send(result)
+    })
   } finally {
     // Uncomment this line if you want to keep the connection open
     // await client.close();
